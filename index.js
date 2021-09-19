2e524a10a8bece2bc2b7e4424ca10fa245d6ebd7
Samantha Mack <heaulin@gmail.com>
2021-09-18 23:35:08 -0700

    code

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const inquirer = require('inquirer')
 
-const questions = [
+const questions = ([
   {
     type: 'input',
     name: 'title',
@@ -26,7 +26,7 @@ const questions = [
     type: 'list',
     name: 'license',
     message: 'Choose your license of choice',
-    choices: ['Mozilla', 'Apache', 'Chrome']
+    choices: ['MIT', 'Apache v2.0']
   },
   {
     type: 'input',
@@ -43,4 +43,52 @@ const questions = [
     name: 'questions'
     message: 'Enter any questions you may have'
   }
-]
+])
+
+.then(answers) => {
+  if(answers.license === 'MIT'){
+    licensebadge = '![License badge] https://img.shields.io/badge/license-MIT-blue'
+    licenseinfo = 'This project is currently using the license MIT'
+  } else if (answers.license === 'Apache v2.0') {
+    licensebadge = '![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg'
+    licenseinfo = "This project is currently using the license Apache v2.0"
+  } 
+}
+
+fs.appendFile('README.md', `
+  # title
+   ${answers.title}
+  # description
+   ${answers.description}
+  # installation
+   ${answers.installation}
+  # usage
+   ${answers.usage}
+  # license
+   ${answers.license}
+  # tests
+   ${answers.tests}
+  # contributing
+   ${answers.contributing}
+   
+   # Questions
+   Additional questions, please contact me on my Github or email:
+   Email: ${answers.email}
+
+   Github: [${answers.username}](https://github.com/${answers.username})
+
+  # table-of-contents
+  
+  1. [title](#title)
+  2. [description](#description)
+  3. [installation](#installation)
+  4. [usage](#usage)
+  5. [license](#license)
+  6. [tests](#tests)
+  7. [contributing](#contributing)
+  8. [questions](#questions)
+  9. [table of contents](#table-of-contents)
+  
+  `, err =>{if(err){console.log(err)}})
+})
+.catch(err=>console.log(err))
